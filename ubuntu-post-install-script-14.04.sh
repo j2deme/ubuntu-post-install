94ba20c0399984a3e2ad5157e08aed8fb039f91e
Jaime Delgado Meraz <j2deme@gmail.com>
2014-07-24 13:15:01 -0500

    Fix minor errors

--- ubuntu-post-install-script-14.04.sh
+++ ubuntu-post-install-script-14.04.sh
@@ -199,6 +199,7 @@ function system {
     sudo apt-get install -y oracle-java8-installer
     echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
     echo 'Done.'
+    ;;
   # Return
   [Rr]*)
     clear && main;;
